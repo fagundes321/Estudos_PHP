33b77f128032f9558f53df475ad39e8295c5f402
fagundes321 <victorfagundes123@gmail.com>
2025-04-30 14:24:32 -0300

    Adicionando generos

--- screen-match.php
+++ screen-match.php
@@ -3,12 +3,29 @@
 echo "Bem-vindo(a) ao screen match!!
 ";
 
-$nomeFilme = "Top Gun - Maverick
-";
-$anoLancamento = 2022;
-$notaFilme = (8.8 + 5 + 4)/2;
+$nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Thor: Ragnarok";
+$nomeFilme = "Watchmen";
+
+$anoLancamento = $argv[1] ?? 2022;
+$notaFilme = (8.8 + 5 + 4)/3;
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020 ;
 
-echo $nomeFilme;
-echo $notaFilme ;
+echo "nome do Filme:  $nomeFilme" ;
+echo "nota do Filme: $notaFilme\n" ;
+echo "Ano de lançamento: $anoLancamento \n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento \n";
+} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022){
+ echo "Esse filme ainda é novo \n";
+} else {
+    echo "Esse filme não é um lançamento \n";
+}
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia",
+};
