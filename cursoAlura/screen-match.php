837462c001a0672ce83d3b0f9f965b7677cc5c6f
fagundes321 <victorfagundes123@gmail.com>
2025-05-18 23:01:31 -0300

    Atualização no screen

--- cursoAlura/screen-match.php
+++ cursoAlura/screen-match.php
@@ -1,47 +1,55 @@
 <?php
 
+function exibeMensagemLancamento($ano) {
+    if ($ano > 2022) {
+        echo "Esse filme é um lançamento!\n";
+    } elseif ($ano > 2020 && $ano <= 2022) {
+        echo "Esse filme ainda é novo.\n";
+    } else {
+        echo "Esse filme não é um lançamento.\n";
+    }
+}
+
+function incluiNoPlano($planoPrime, $anoLancamento) {
+   return $planoPrime || $anoLancamento < 2020;
+}
+
 echo "Bem-vindo(a) ao Screen Match!!\n";
 
-$nomeFilme = "Top Gun - Maverick\n";
-
+$nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
 
-$quantidadeDeNotas = $argc -1;
+$quantidadeDeNotas = $argc - 1;
 $notas = [];
 
-for ($contador = 1; $contador < $argc; $contador++) { 
+for ($contador = 1; $contador < $argc; $contador++) {
     $notas[] = (float) $argv[$contador];
 }
 
-$somasDeNotas = 0;
-foreach ($notas as $nota){ 
+$somaDeNotas = 0;
+foreach ($notas as $nota) {
     $somaDeNotas += $nota;
 }
 
-var_dump($notas);
- 
+
 $notaFilme = array_sum($notas) / $quantidadeDeNotas;
+
 $planoPrime = true;
-$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+$incluidoNoPlano = incluiNoPlano($planoPrime, $anoLancamento);
 
 
 
-echo "Nome do Filme: $nomeFilme";
+exibeMensagemLancamento($anoLancamento);
+
+echo "Nome do Filme: $nomeFilme\n";
 echo "Nota do Filme: $notaFilme\n";
 echo "Ano de Lançamento: $anoLancamento\n";
 
-if ($anoLancamento > 2022) {
-    echo "Esse filme é um lançamento!\n";
-} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
-    echo "Esse filme ainda é novo.\n";
-} else {
-    echo "Esse filme não é um lançamento.\n";
-}
-
 $genero = match (trim($nomeFilme)) {
     "Top Gun - Maverick" => "Ação",
-    // "Thor: Ragnarok" => "Super-herói",
-    // "Se Beber Não Case" => "Comédia",
+    "Thor: Ragnarok" => "Super-herói",
+    "Se Beber Não Case" => "Comédia",
     default => "Gênero desconhecido",
 };
 
