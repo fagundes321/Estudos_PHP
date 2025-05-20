af57fa888aa22b2fa01e5ee4d4165a850754ec08
fagundes321 <victorfagundes123@gmail.com>
2025-05-20 17:03:14 -0300

    Metodo Construtor

--- Caneta.php
+++ Caneta.php
@@ -1,27 +1,45 @@
 <?php
 
-class Caneta{
-    // meus atributos, tudo aquilo que é exclusivo do objeto
-    public $modelo;
-    public $cor;
+class Caneta
+{
+    private  $modelo;
+    private $cor;
     private $ponta;
-    protected $carga;
-    protected $tampada;
+    private $tampada;
 
-    // Metodos, tudo aquilo que o objeto pode fazer
-    public function rabiscar() {
-        if ($this -> tampada == true) {
-            echo "<p>ERRO! Não posso rabiscar!</p>";
-        }else {
-            echo "<p>Estou rabiscando...</p>";
-        }
+    public function __construct($m, $c, $p) { //metodo construtor
+       $this->modelo = $m;
+       $this->cor = $c;
+       $this->ponta = $p;
+       $this->tampar();
+       
     }
-
+    
+       
     public function tampar() {
         $this->tampada = true;
     }
 
-    public function destampar() {
-        $this->tampada = false;
+    
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function setModelo($m)
+    {
+        $this->modelo = $m;
+
+    }
+
+    public function getPonta()
+    {
+        return $this->ponta;
+    }
+
+    public  function setPonta($p)
+    {
+       $this->ponta = $p;
     }
 }
--- index.php
+++ index.php
@@ -3,48 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
 <body>
     <pre>
         <?php
         
             require_once 'Caneta.php';
-            // $c1 = new Caneta;
-            // $c1 -> cor = "Azul";
-            // $c1 -> ponta = 0.5;
-            // $c1 -> tampada = false;
-            // $c1 -> tampar();
-            // print_r($c1);
-
-            // echo "<br>";
-
-            // $c2 = new Caneta;
-            // $c2 -> cor = "Verde";
-            // $c2 -> carga = 50;
-            // $c2 -> tampar();
-            // print_r($c2);
-
-            // echo "<br>";
-
-            // require_once "Impressora.php";
-            // $imp1 = new Impressora;
-            // $imp1 -> papel = 50;
-            // $imp1 -> tinta = 30;
-            // $imp1 -> ligado = true;
-            // $imp1 -> ligar();
-            // print_r($imp1);
-
-            $c1 = new Caneta;
-            $c1 -> modelo = "BIC cristal";
-            $c1 -> cor = "Azul";
-            //$c1 -> ponta = 0.5;
-            // $c1 -> carga = 99;
-            // $c1 -> tampada = true;
+            $c1 = new Caneta("BIC", "Azul", 0.5);
             
-            $c1 -> rabiscar();
-            $c1 -> tampar();        
-            print_r($c1);
+            echo '<pre>'; 
+            print_r($c1); 
+            echo '</pre>'; 
             ?>
     </pre>
 </body>
