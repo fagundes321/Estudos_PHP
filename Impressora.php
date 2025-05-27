5049be24fe1c822a9031b01fe52ac4e0d73d33f4
fagundes321 <victorfagundes123@gmail.com>
2025-05-26 23:56:58 -0300

    Melhorado a impressora

--- Impressora.php
+++ Impressora.php
@@ -1,27 +1,81 @@
 <?php
-class Impressora{
-    public $tinta;
-    public $papel;
-    public $ligado;
+// class Impressora{
+//     public $tinta;
+//     public $papel;
+//     public $ligado;
 
-    function ligar(){
-        if ($this -> ligado == true) {
-            echo "Pronta para Imprimir";
-        }else{
-            Echo "Erro! A impressora não está ligada";
+//     function ligar(){
+//         if ($this -> ligado == true) {
+//             echo "Pronta para Imprimir";
+//         }else{
+//             Echo "Erro! A impressora não está ligada";
+//         }
+//     }
+
+
+//     function receber(){
+
+//     }
+
+//     function imprimir(){
+
+//     }
+
+//     function escanear(){
+
+//     }
+// }
+
+<?php
+class Impressora {
+    public $tinta = 100;
+    public $papel = 10;
+    public $ligado = false;
+
+    function ligar() {
+        if (!$this->ligado) {
+            $this->ligado = true;
+            echo "Impressora ligada.\n";
+        } else {
+            echo "A impressora já está ligada.\n";
         }
     }
 
-
-    function receber(){
-
+    function receber($documento) {
+        if ($this->ligado) {
+            echo "Documento '{$documento}' recebido para impressão.\n";
+        } else {
+            echo "Erro! A impressora está desligada.\n";
+        }
     }
 
-    function imprimir(){
+    function imprimir() {
+        if (!$this->ligado) {
+            echo "Erro! A impressora está desligada.\n";
+            return;
+        }
 
+        if ($this->papel > 0 && $this->tinta >= 10) {
+            $this->papel--;
+            $this->tinta -= 10;
+            echo "Imprimindo documento...\n";
+        } else {
+            echo "Erro! Papel ou tinta insuficiente.\n";
+        }
     }
 
-    function escanear(){
-
+    function escanear() {
+        if ($this->ligado) {
+            echo "Escaneando documento...\n";
+        } else {
+            echo "Erro! A impressora está desligada.\n";
+        }
     }
 }
+
+// Exemplo de uso:
+$minhaImpressora = new Impressora();
+$minhaImpressora->ligar();
+$minhaImpressora->receber("Relatório Semanal");
+$minhaImpressora->imprimir();
+$minhaImpressora->escanear();
