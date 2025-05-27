b0c40933d8feeea699c1f18830938ca5f74c108f
fagundes321 <victorfagundes123@gmail.com>
2025-05-27 16:33:47 -0300

    função criar filme

--- Caneta.php
+++ Caneta.php
@@ -7,20 +7,21 @@ class Caneta
     private $ponta;
     private $tampada;
 
-    public function __construct($m, $c, $p) { //metodo construtor
-       $this->modelo = $m;
-       $this->cor = $c;
-       $this->ponta = $p;
-       $this->tampar();
-       
+    public function __construct($m, $c, $p)
+    { //metodo construtor
+        $this->modelo = $m;
+        $this->cor = $c;
+        $this->ponta = $p;
+        $this->tampar();
     }
-    
-       
-    public function tampar() {
+
+
+    public function tampar()
+    {
         $this->tampada = true;
     }
 
-    
+
 
     public function getModelo()
     {
@@ -30,7 +31,6 @@ class Caneta
     public function setModelo($m)
     {
         $this->modelo = $m;
-
     }
 
     public function getPonta()
@@ -40,6 +40,6 @@ class Caneta
 
     public  function setPonta($p)
     {
-       $this->ponta = $p;
+        $this->ponta = $p;
     }
 }
--- screen-macth/screen-match.php
+++ screen-macth/screen-match.php
@@ -36,12 +36,8 @@ $genero = match (trim($nomeFilme)) {
 
 echo "O gênero do filme é: $genero\n";
 
-$filme = [
-    "nome" => "Thor: Ragnarok",
-    "ano" => 2021,
-    "nota" => 7.8,
-    "genero" => "super-herói",
-];
+$filme = criaFilme(nome : "Thor: Ragnarok", anoLancamento: 2021 ,nota: 7.8, genero: "super-herói");
+
 
 echo $filme["ano"];
 
