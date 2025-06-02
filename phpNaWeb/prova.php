19e80fc300c13bf3ee13ae9181d39b38c03004b1
fagundes321 <victorfagundes123@gmail.com>
2025-06-02 00:53:44 -0300

    Melhoras no prova

--- phpNaWeb/prova.php
+++ phpNaWeb/prova.php
@@ -4,21 +4,20 @@ $a = 0;
 $b = 2;
 $c = 1;
 
-if ($a > 0 ) {
-    if ($b > 0 ) {
-        echo "letra E";
-    }else {
-        echo "letra D";
+// Estrutura condicional para imprimir letras com base nos valores de $a, $b e $c
+if ($a > 0) {
+    if ($b > 0) {
+        echo "Letra E" . PHP_EOL;
+    } else {
+        echo "Letra D" . PHP_EOL;
     }
 } else {
     if ($c > 0) {
-        echo "letra B\n";
+        echo "Letra B" . PHP_EOL;
     }
 }
 
-$i = 0;
-
-while ($i <= 10) {
-    echo($i . " Ola mundo\n");
-    $i = $i + 1;
+// Estrutura de repetição para imprimir de 0 a 10 com a mensagem "Olá mundo"
+for ($i = 0; $i <= 10; $i++) {
+    echo "$i Olá mundo" . PHP_EOL;
 }
